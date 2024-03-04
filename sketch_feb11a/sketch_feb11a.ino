6478bf3571fdc82b7398bfdd53c7d9fd5d82292a
Adrian V <117219790+adryvlz@users.noreply.github.com>
2024-03-04 00:50:37 -0500

    Update sketch_feb11a.ino with barometric sensor
    5 second data recording interval, can be changed to be 10 ms as stated by Maddy

--- sketch_feb11a/sketch_feb11a.ino
+++ sketch_feb11a/sketch_feb11a.ino
@@ -1,8 +1,19 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+#define seaLevelPressure_hPa 1013.25
+
+Adafruit_BMP085 bmp;
+
 const int outputPin = 6;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(outputPin,OUTPUT);
+  if (!bmp.begin()) {
+  Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+  while (1) {}
+  }
 }
 
 void loop() {
@@ -11,6 +22,31 @@ void loop() {
   delay(2000);
   digitalWrite(outputPin,LOW);
   delay(2000);
+
+  // for the pressure and temperature sensor
+  Serial.print("Temperature = ");
+  Serial.print(bmp.readTemperature());
+  Serial.println(" *C");
+    
+  Serial.print("Pressure = ");
+  Serial.print(bmp.readPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Altitude = ");
+  Serial.print(bmp.readAltitude());
+  Serial.println(" meters");
+
+  Serial.print("Pressure at sealevel (calculated) = ");
+  Serial.print(bmp.readSealevelPressure());
+  Serial.println(" Pa");
+
+  Serial.print("Real altitude = ");
+  Serial.print(bmp.readAltitude(seaLevelPressure_hPa * 100));
+  Serial.println(" meters");
+
+  // use delay 1 for every 10 ms as stated by maddy 
+  Serial.println();
+  delay(500);
 }
 void startCamera() {
   signal();
