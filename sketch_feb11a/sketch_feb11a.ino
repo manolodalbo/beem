18bfd13f7de8790a27324da7dbcea9d85d76eb33
manolodalbo <manuel_dal_bo@brown.edu>
2024-03-07 17:46:58 -0500

    finished initial draft of accelorometer code

--- sketch_feb11a/sketch_feb11a.ino
+++ sketch_feb11a/sketch_feb11a.ino
@@ -1,16 +1,26 @@
-const int outputPin = 6;
+#include <Adafruit_Sensor.h>
+#include <Adafruit_ADXL345_U.h>
+#include <Wire.h>
+
+
+const int cameraTriggerPin = 6;
+Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified();
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
-  pinMode(outputPin,OUTPUT);
+  pinMode(cameraTriggerPin,OUTPUT);
+  if (!accel.begin()) {
+    Serial.println("No accelerometer detected");
+    while(1);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(outputPin,HIGH);
-  delay(2000);
-  digitalWrite(outputPin,LOW);
-  delay(2000);
+  float a = getYAcceleration();
+  while(a>5.00) {
+    startCamera();
+    delay(10000);
+  }
 }
 void startCamera() {
   signal();
@@ -21,7 +31,12 @@ void endCamera() {
 }
 
 void signal() {
-  digitalWrite(outputPin,HIGH);
-  delay(10000);
-  digitalWrite(outputPin,LOW);
+  digitalWrite(cameraTriggerPin,HIGH);
+  delay(200);
+  digitalWrite(cameraTriggerPin,LOW);
+}
+float getYAcceleration() {
+  sensors_event_t event;
+  accel.getEvent(event);
+  return event.acceleration.y;
 }
